7e2b1fc7a6ca74f8c7b3e6303a2e789ac1f4172a
Aditya Ekhande <aadiekhande@gmail.com>
2024-09-29 23:35:02 +0530

    upload page changes

--- src/components/Publications/Publications.jsx
+++ src/components/Publications/Publications.jsx
@@ -1,6 +1,26 @@
 import React from 'react';
+import { useEffect, useState } from "react";
 import Navbar from '../navbar';
 import Footer from '../Footer'
+
+//To fetch latest three PDF
+// useEffect(() => {
+//     const fetchLatestPDFs = async () => {
+//         try {
+//             const response = await fetch(`${MAIN_URL}/getLatest3PDF`); // Adjust the URL if needed
+//             if (!response.ok) {
+//                 throw new Error("Failed to fetch PDFs");
+//             }
+//             const data = await response.json();
+//             setPdfs(data);
+//         } catch (err) {
+//             setError(err.message);
+//         }
+//     };
+
+//     fetchLatestPDFs();
+// }, []);
+
 
 function Publications(props) {
     return (
@@ -10,39 +30,20 @@ function Publications(props) {
                 <h1>Latest Publications</h1>
                 <div>
                     <h3>Vol.1</h3>
-                    <ol>
-                        <li>Publication 1</li>
-                        <button>View Paper</button>
-                        <li>Publication 2</li>
-                        <button>View Paper</button>
-                        <li>Publication 3</li>
-                        <button>View Paper</button>
-                    </ol>
+                    
+                        {/* <ul>
+                            {pdfs.map((pdf, index) => (
+                                <li key={index}>
+                                    <button style={{ margin: 5, padding: 3 }} onClick={() => fetchAnyPDF(pdf.path)}>
+                                        Publication {index + 1}
+                                    </button>
+                                </li>
+                            ))}
+                        </ul> */}
+                
                 </div>
                 <hr />
-                <div>
-                    <h3>Vol.2</h3>
-                    <ol>
-                        <li>Publication 1</li>
-                        <button>View Paper</button>
-                        <li>Publication 2</li>
-                        <button>View Paper</button>
-                        <li>Publication 3</li>
-                        <button>View Paper</button>
-                    </ol>
-                </div>
-                <hr />
-                <div>
-                    <h3>Vol.3</h3>
-                    <ol>
-                        <li>Publication 1</li>
-                        <button>View Paper</button>
-                        <li>Publication 2</li>
-                        <button>View Paper</button>
-                        <li>Publication 3</li>
-                        <button>View Paper</button>
-                    </ol>
-                </div>
+
             </div>
             <Footer />
         </>
