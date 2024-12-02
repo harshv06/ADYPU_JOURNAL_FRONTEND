0f764abc81c833595f2a6663a839c9a43f276b4c
harshv06 <harshvonmail@gmail.com>
2024-12-02 21:56:05 +0530

    Minor changes 2

--- src/components/PublishinHouse/PublishingHouse.jsx
+++ src/components/PublishinHouse/PublishingHouse.jsx
@@ -10,7 +10,7 @@ function PublishingHouse() {
           <div className="col-md-8 text-justify align-self-center">
             <h1 style={{fontWeight:'400'}}>All Correspondence to,</h1>
             <h1 className="font-weight-bold ">Publisher</h1>
-            <h1 style={{fontSize:'30px'}}>Dr Farook Sayyad</h1>
+            <h1 style={{fontSize:'30px'}}>Dr. Farook Sayyad</h1>
             <h4 className="text-justify mb-4">
               <span style={{color:'purple', fontWeight:'bold',fontSize:'22px'}}>Ajeenkya DY Patil School of Engineering</span><br />
               Ajeenkya DY Patil University, DY Patil Knowledge City Road, Via Lohegaon, Airport Rd,
