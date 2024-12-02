c137774255def26c70cad94715c98ffc11d62ad4
harshv06 <harshvonmail@gmail.com>
2024-12-02 15:51:24 +0530

    Minor changes 2

--- src/components/PublishinHouse/PublishingHouse.jsx
+++ src/components/PublishinHouse/PublishingHouse.jsx
@@ -1,21 +1,26 @@
 import React from 'react';
 import Navbar from '../navbar';
 
-
 function PublishingHouse() {
   return (
     <>
       <Navbar />
       <div className="container my-5">
         <div className="row justify-content-center">
-          <div className="col-md-8 text-center">
-            <h1 className="mb-4">All Correspondence to: Publisher</h1>
-            
+          <div className="col-md-8 text-justify align-self-center">
+            <h1 style={{fontWeight:'400'}}>All Correspondence to,</h1>
+            <h1 className="font-weight-bold ">Publisher</h1>
+            <h1 style={{fontSize:'30px'}}>Dr Farook Sayyad</h1>
             <h4 className="text-justify mb-4">
-              Ajeenkya D Y Patil School of Engineering<br />
+              <span style={{color:'purple', fontWeight:'bold',fontSize:'22px'}}>Ajeenkya DY Patil School of Engineering</span><br />
               Ajeenkya DY Patil University, DY Patil Knowledge City Road, Via Lohegaon, Airport Rd,
               Charholi Budruk, Pune, Maharashtra 412105, India
             </h4>
+
+            {/* Publisher name with style applied */}
+            {/* <h4 style={{ color: 'purple', fontWeight: 'bold',fontSize:'20px' }}>
+              Publisher: Ajeenkya DY Patil School of Engineering
+            </h4> */}
 
             <div>
               <h4>Email: 
